5874b39c01b2779e2e4ae0c8801ad50e18618680
Brian Vaughn <brian@recurly.com>
2014-07-13 11:01:13 -0700

    Usability tweak

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Chrome extension enabling automatic downloads of any SoundCloud track, even when
 
 This extension is not currently available in the Chrome web store, so you will need to follow [Google's instructions for how to load an unpacked extension](https://developer.chrome.com/extensions/getstarted#unpacked).
 
-Once you've installed the instruction, browse to [any SoundCloud song-page](https://soundcloud.com/brianvaughn/burn-you-away), right click, and select *SoundClouDownload all Mp3s*. At this point any SoundCloud song-page you visit (including the one you were already one) will be automatically downloaded to your *Downloads* directory.
-
-To deactivate the plugin, right click and select *Stop SoundClouDownloading*.
+Once you've installed the instruction, browse to [any SoundCloud song-page](https://soundcloud.com/brianvaughn/burn-you-away), right click, and select *Download SoundCloud track*. The SoundCloud track should automatically downloaded to your *Downloads* directory.
 
 ## Requests? Suggestions?
 
--- src/background.js
+++ src/background.js
@@ -1,54 +1,27 @@
-var START_DOWNLOADING_CONTEXT_MENU_ID = 'startDownloadingContextMenu';
-var STOP_DOWNLOADING_CONTEXT_MENU_ID = 'stopDownloadingContextMenu';
-
-// Track files downloaded to prevent duplicates.
-var downloadedFiles = [];
+var START_DOWNLOAD_CONTEXT_MENU = 'startDownloadingContextMenu';
 
 var showStartDownloadingContextMenu = function() {
-  try {
-    chrome.contextMenus.remove(STOP_DOWNLOADING_CONTEXT_MENU_ID);
-  } catch (error) {}
-
   chrome.contextMenus.create({
-    title: 'SoundClouDownload all Mp3s',
+    title: 'Download SoundCloud track',
     contexts: ['page'],
-    id: START_DOWNLOADING_CONTEXT_MENU_ID
-  });
-};
-
-var showStopDownloadingContextMenu = function() {
-  try {
-    chrome.contextMenus.remove( START_DOWNLOADING_CONTEXT_MENU_ID);
-  } catch (error) {}
-
-  chrome.contextMenus.create({
-    title: 'Stop SoundClouDownloading',
-    contexts: ['page'],
-    id: STOP_DOWNLOADING_CONTEXT_MENU_ID
+    id: START_DOWNLOAD_CONTEXT_MENU,
+    documentUrlPatterns: ['*://soundcloud.com/*']
   });
 };
 
 var onBeforeRequestHandler = function(details) {
   var mp3 = details.url;
 
-  console.info('Caught request, stopping playback...');
-
-  soundCloudDOMHelper.getPauseButton().click();
-
-  if (downloadedFiles[mp3]) {
-    console.info('Already downloaded this song; skipping...');
-
-    return;
-  }
-
-  downloadedFiles[mp3] = true;
-
   console.info('Downloading mp3:', mp3);
 
   chrome.downloads.download({url: mp3});
+
+  stopInterceptingMp3Requests();
 };
 
 var startInterceptingMp3Requests = function() {
+  console.info('Registering Mp3 intercept handler');
+
   chrome.webRequest.onBeforeRequest.addListener(
     onBeforeRequestHandler,
     {urls: ['*://*/*.mp3?*']},
@@ -56,57 +29,20 @@ var startInterceptingMp3Requests = function() {
 };
 
 var stopInterceptingMp3Requests = function() {
+  console.info('Removing Mp3 intercept handler');
+
   chrome.webRequest.onBeforeRequest.removeListener(onBeforeRequestHandler);
-};
-
-var findAndRestartSoundCloudTabs = function(url) {
-  console.log('Looking for SoundCloud tabs...');
-
-  chrome.tabs.query({
-    active: true,
-    currentWindow: true,
-    url: url
-  },
-  function(tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-      var tab = tabs[i];
-
-      console.log('Restarting tab #' + tab.index + ' at ' + tab.url);
-
-      chrome.tabs.executeScript(tab.id, {
-        code: 'window.location.reload();'
-      });
-    }
-  });
-};
-
-// Helper methods for interacting with SoundCloud DOM
-var soundCloudDOMHelper = {
-  hasListenContext: function() {
-    return this.getListenContext().length > 0;
-  },
-  getListenContext: function() {
-    return $('.sound.single.listenContext');
-  },
-  getPlayButton: function() {
-    return this.getListenContext().find('button[title="Play"]');
-  },
-  getPauseButton: function() {
-    return this.getListenContext().find('button[title="Pause"]');
-  }
 };
 
 showStartDownloadingContextMenu();
 
 chrome.contextMenus.onClicked.addListener(
-  function(data) {
-    if (data.menuItemId === START_DOWNLOADING_CONTEXT_MENU_ID) {
-      showStopDownloadingContextMenu();
+  function(menuItem, tab) {
+    if (menuItem.menuItemId === START_DOWNLOAD_CONTEXT_MENU) {
       startInterceptingMp3Requests();
-      findAndRestartSoundCloudTabs(data.pageUrl);
 
-    } else if (data.menuItemId === STOP_DOWNLOADING_CONTEXT_MENU_ID) {
-      showStartDownloadingContextMenu();
-      stopInterceptingMp3Requests();
+      chrome.tabs.executeScript(tab.id, {
+        code: 'window.location.reload();'
+      });
     }
   });
